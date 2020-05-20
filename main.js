ea0bd3325beec0f3fed1379672e4dbea214962f4
Momo Ramadori <momoramadori8@gmail.com>
2020-05-20 16:03:00 +0200

    aggiunti commenti

--- main.js
+++ main.js
@@ -7,8 +7,10 @@ $.ajax({
     'success': function(data) {
         var source   = $("#template").html();
         var template = Handlebars.compile(source);
+        //utilizzo un ciclo per scorrere tutti gli oggetti dentro l'array response che è una chiave di data
         for (var i = 0; i < data.response.length; i++) {
             console.log(data.response[i]);
+            //creo un oggetto a cui attribuisco i valori delle singole chiavi egli oggetti di response
             var context = {
                 'src' : data.response[i].poster,
                 'title': data.response[i].title,
@@ -16,11 +18,18 @@ $.ajax({
                 'genre': data.response[i].genre,
                 'year': data.response[i].year
             }
+            //bonus select generi
+            //quando cambio il select
             $('#generi').change(function() {
+                //rendo tutti invisibili
                 $('.cd').removeClass('active');
+                // prendo ogni opzione selezionata
                 $('#generi option:selected').each(function(){
+                    //creo una variabile per salvare il genere dell'opzione selezionata
                     var genere = $(this).attr('value');
+                    //identifico i cd che hanno lo stesso genere e li rendo visibili
                     $('.cd[value ='+genere+']').addClass('active');
+                    //se l'utente clicca su All tutti tornano visibili
                     if($(this).text() == 'All') {
                         $('.cd').addClass('active')
                     }
@@ -28,7 +37,7 @@ $.ajax({
             });
             var html = template(context);
             $('main').append(html)
-        }
+        };
     },
     'error': function() {
         alert('ops.. qualcosa è andato storto!')
