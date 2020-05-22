fa82d68b127360dfebf478ede0411bcad38f8b5d
Momo Ramadori <momoramadori8@gmail.com>
2020-05-22 11:54:19 +0200

    migliorie

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
-var source   = $("#template").html();
-var template = Handlebars.compile(source);
+$(document).ready(function(){
+    var source   = $("#template").html();
+    var template = Handlebars.compile(source);
 
-$.ajax({
-    'url': 'https://flynn.boolean.careers/exercises/api/array/music',
-    'method': 'GET',
-    'success': function(data) {
-        //utilizzo un ciclo per scorrere tutti gli oggetti dentro l'array response che è una chiave di data
-        for (var i = 0; i < data.response.length; i++) {
-            console.log(data.response[i]);
-            //creo un oggetto a cui attribuisco i valori delle singole chiavi egli oggetti di response
-            var context = {
-                'src' : data.response[i].poster,
-                'title': data.response[i].title,
-                'author': data.response[i].author,
-                'genre': data.response[i].genre,
-                'year': data.response[i].year
-            }
-            //bonus select generi
-            //quando cambio il select
-            $('#generi').change(function() {
-                //rendo tutti invisibili
-                $('.cd').removeClass('active');
-                // leggo il valore della select
-                var genere = $(this).val();
-                //trovo i cd che hanno lo stesso value e li rendo visibili
-                $('.cd[value ='+genere+']').addClass('active');
-                //se il valore è all li rendo tutti visibili
-                if(genere == 'All') {
-                    $('.cd').addClass('active')
-                }
-            });
-            var html = template(context);
-            $('main').append(html)
-        };
-    },
-    'error': function() {
-        alert('ops.. qualcosa è andato storto!')
+    //chiamata ajax per recuperare i dati dall'API
+    $.ajax({
+        'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+        'method': 'GET',
+        'success': function(data) {
+            //utilizzo un ciclo per scorrere tutti gli oggetti dentro l'array response che è una chiave di data
+            gestisci_dati(data.response)
+        },
+        'error': function() {
+            alert('ops.. qualcosa è andato storto!')
+        }
+    })
+    //bonus select generi
+    //quando cambio il select
+    $('#generi').change(function() {
+        //rendo tutti invisibili
+        $('.cd').removeClass('active');
+        // leggo il valore della select
+        var genere = $(this).val();
+        //trovo i cd che hanno lo stesso value e li rendo visibili
+        $('.cd[value ='+genere+']').addClass('active');
+        //se il valore è all li rendo tutti visibili
+        if(genere == 'All') {
+            $('.cd').addClass('active')
+        }
+    })
+
+    //funzione per disegnare il disco in pagina
+    function disegna_dischi(oggetto) {
+        var context = {
+            'src' : oggetto.poster,
+            'title': oggetto.title,
+            'author': oggetto.author,
+            'genre': oggetto.genre,
+            'year': oggetto.year
+        }
+        var html = template(context);
+        $('main').append(html)
     }
-})
+
+    //funzione per ciclare tutti i dischi dischi
+    function gestisci_dati(dischi) {
+        for (var i = 0; i < dischi.length; i++) {
+            var disco = dischi[i];
+            disegna_dischi(disco)
+        }
+    }
+});
